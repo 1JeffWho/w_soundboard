a05be99c82576bbb91e640dfcbe5488edd1f1875
1JeffWho <94126816+1JeffWho@users.noreply.github.com>
2023-05-06 20:30:59 +0200

    Add files via upload

--- script.js
+++ script.js
@@ -1,5 +1,12 @@
+const songs = ['bimmm', 'bistdudeppert', 'danke', 'eini do', 'haha', 'haudieiniafoch', 'hunger', 'i fixed it', 'schnuppie', 'schwupp', 'so_patrick_lets_go', 'und eini', 'watsi watsi', 'woaahh', 'woopsie woopsie'];
 
 function playSound(sound) {
+    if (sound == "random"){
+        let a = Math.floor(Math.random() * songs.length);
+        sound = songs[a];
+        console.log(songs[a]);
+    }
+
     var audio = new Audio('/sounds/' + sound + '.mp3');
     audio.play();
 }
